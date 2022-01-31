06ab67463d102932e8cd4ce5e167174aceb423e7
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-01-31 15:34:44 -0300

    Add CodeDomService

--- RevitAddin.CodeCompileTest/Revit/Commands/Command.cs
+++ RevitAddin.CodeCompileTest/Revit/Commands/Command.cs
@@ -1,8 +1,10 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+
+using RevitAddin.CodeCompileTest.Services;
 using System;
-using System.CodeDom;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -15,29 +17,39 @@ namespace RevitAddin.CodeCompileTest.Revit.Commands
         {
             UIApplication uiapp = commandData.Application;
 
-            NewCodeDomClass.BuildHelloWorldGraph();
+            CodeDomService newCodeDomClass = new CodeDomService();
+            Assembly assembly = newCodeDomClass.CreateCommand();
 
-            foreach (var item in Assembly.GetExecutingAssembly().GetTypes())
+            foreach (var type in assembly.GetTypes())
             {
-                Console.WriteLine(item);
+                Console.WriteLine($"Type: {type}");
             }
 
-            //new RvtView().ShowDialog();
-
-            foreach (var item in Autodesk.Windows.ComponentManager.QuickAccessToolBar.Items)
+            var commands = assembly.GetTypes().Where(e => typeof(IExternalCommand).IsAssignableFrom(e));
+            foreach (var command in commands)
             {
-                Console.WriteLine($"{item.AutomationName} {item.Id}");
+                Console.WriteLine($"Command: {command}");
+                var name = $"{command} {DateTime.Now}";
+                var text = $"Command";
+                PushButtonData currentBtn = NewPushButton(command, name, text);
+
+                App.ribbonPanel.AddItem(currentBtn);
             }
-
-            var items = Autodesk.Windows.ComponentManager.QuickAccessToolBar.Items;
-            var i = items.FirstOrDefault(e => e.AutomationName == "App");
-
-            if (i != null)
-                Autodesk.Windows.ComponentManager.QuickAccessToolBar.Items.Remove(i);
-
 
 
             return Result.Succeeded;
+        }
+
+        private PushButtonData NewPushButton(Type command, string name = null, string text = null)
+        {
+            var commandType = command;
+            var currentDll = commandType.Assembly.Location;
+            string fullname = commandType.FullName;
+            string targetName = commandType.Name;
+            if (name != null) targetName = name;
+            PushButtonData currentBtn = new PushButtonData(targetName, targetName, currentDll, fullname);
+            if (text != null) currentBtn.Text = text;
+            return currentBtn;
         }
     }
 
@@ -53,66 +65,6 @@ namespace RevitAddin.CodeCompileTest.Revit.Commands
             System.Windows.MessageBox.Show(t.Name);
 
             return Result.Succeeded;
-        }
-    }
-
-    class NewCodeDomClass
-    {
-        CodeCompileUnit targetUnit;
-        CodeTypeDeclaration targetClass;
-        public NewCodeDomClass(string nameassembly)
-        {
-            targetUnit = new CodeCompileUnit();
-            CodeNamespace samples = new CodeNamespace(nameassembly);
-            samples.Imports.Add(new CodeNamespaceImport("Methods.ClassWithIExternalCommand"));
-            targetClass = new CodeTypeDeclaration(nameassembly);
-            targetClass.IsClass = true;
-            targetClass.TypeAttributes = TypeAttributes.Public;
-            targetClass.BaseTypes.Add(typeof(Autodesk.Revit.UI.IExternalCommand));
-            samples.Types.Add(targetClass);
-            targetUnit.Namespaces.Add(samples);
-        }
-
-        // Build a Hello World program graph using
-        // System.CodeDom types.
-        public static CodeCompileUnit BuildHelloWorldGraph()
-        {
-            // Create a new CodeCompileUnit to contain
-            // the program graph.
-            CodeCompileUnit compileUnit = new CodeCompileUnit();
-
-            // Declare a new namespace called Samples.
-            CodeNamespace samples = new CodeNamespace("Samples");
-            // Add the new namespace to the compile unit.
-            compileUnit.Namespaces.Add(samples);
-
-            // Add the new namespace import for the System namespace.
-            samples.Imports.Add(new CodeNamespaceImport("System"));
-
-            // Declare a new type called Class1.
-            CodeTypeDeclaration class1 = new CodeTypeDeclaration("Class1");
-            // Add the new type to the namespace type collection.
-            samples.Types.Add(class1);
-
-            // Declare a new code entry point method.
-            CodeEntryPointMethod start = new CodeEntryPointMethod();
-
-            // Create a type reference for the System.Console class.
-            CodeTypeReferenceExpression csSystemConsoleType = new CodeTypeReferenceExpression("System.Console");
-
-            // Build a Console.WriteLine statement.
-            CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
-                csSystemConsoleType, "WriteLine",
-                new CodePrimitiveExpression("Hello World!"));
-
-            // Add the WriteLine call to the statement collection.
-            start.Statements.Add(cs1);
-
-            // Add the code entry point method to
-            // the Members collection of the type.
-            class1.Members.Add(start);
-
-            return compileUnit;
         }
     }
 }
---
+++ RevitAddin.CodeCompileTest/Services/CodeDomService.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RevitAddin.CodeCompileTest.Services
+{
+    public class CodeDomService
+    {
+        public Assembly CreateCommand()
+        {
+            string source = @"
+            using Autodesk.Revit.Attributes;
+            using Autodesk.Revit.DB;
+            using Autodesk.Revit.UI;
+            namespace CodeNamespace 
+            {
+              [Transaction(TransactionMode.Manual)]
+              public class Command : IExternalCommand
+              {     
+                public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+                {
+                    UIApplication uiapp = commandData.Application;
+
+                    System.Windows.MessageBox.Show(this.GetType().Name + '\n' + this.GetType().Assembly.GetName().Name);
+
+                    return Result.Succeeded;
+                }
+              }
+              public class Teste {}
+            }";
+
+            var csu0 = new CodeSnippetCompileUnit(source);
+
+            return GenerateCode(csu0);
+        }
+
+        public CodeCompileUnit BuildHelloWorldGraph()
+        {
+
+            CodeCompileUnit targetUnit = new CodeCompileUnit();
+
+            // Declare a new namespace called Samples.
+            CodeNamespace samples = new CodeNamespace("Samples");
+            // Add the new namespace to the compile unit.
+            targetUnit.Namespaces.Add(samples);
+
+            // Add the new namespace import for the System namespace.
+            samples.Imports.Add(new CodeNamespaceImport("System"));
+
+            // Declare a new type called Class1.
+            CodeTypeDeclaration class1 = new CodeTypeDeclaration("Class1");
+            // Add the new type to the namespace type collection.
+            samples.Types.Add(class1);
+
+            // Declare a new code entry point method.
+            CodeEntryPointMethod start = new CodeEntryPointMethod();
+
+            // Create a type reference for the System.Console class.
+            CodeTypeReferenceExpression csSystemConsoleType = new CodeTypeReferenceExpression("System.Console");
+
+            // Build a Console.WriteLine statement.
+            CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
+                csSystemConsoleType, "WriteLine",
+                new CodePrimitiveExpression("Hello World!"));
+
+            // Add the WriteLine call to the statement collection.
+            start.Statements.Add(cs1);
+
+            // Add the code entry point method to
+            // the Members collection of the type.
+            class1.Members.Add(start);
+
+            return targetUnit;
+        }
+
+        public Assembly GenerateCode(params CodeCompileUnit[] compilationUnits)
+        {
+            CodeDomProvider provider = new CSharpCodeProvider();
+            CompilerParameters compilerParametes = new CompilerParameters();
+
+            compilerParametes.GenerateExecutable = false;
+            compilerParametes.IncludeDebugInformation = false;
+            compilerParametes.GenerateInMemory = false;
+
+            #region Add GetReferencedAssemblies
+            var assemblyNames = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+            var nameAssemblies = new Dictionary<string, Assembly>();
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (assemblyNames.Any(e => e.Name == assembly.GetName().Name))
+                {
+                    nameAssemblies[assembly.GetName().Name] = assembly;
+                }
+            }
+            foreach (var keyAssmbley in nameAssemblies)
+            {
+                compilerParametes.ReferencedAssemblies.Add(keyAssmbley.Value.Location);
+            }
+            #endregion
+
+            CompilerResults results = provider.CompileAssemblyFromDom(compilerParametes, compilationUnits);
+            return results.CompiledAssembly;
+        }
+    }
+}
